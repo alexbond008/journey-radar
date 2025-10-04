e4bd990a72c26576550cb6b696871137d55f62a5
FloudMe77 <marecik.dariusz@gmail.com>
2025-10-04 19:32:13 +0200

    add hardcode

--- fastapi_app/models/schemes.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from typing import List, Optional
-from datetime import datetime
-from enum import Enum
-
-# This file is now empty - all models have been moved to database_models.py
-# Import from database_models.py instead:
-# from models.database_models import User, Train, Stop, Line, Edge, Event, LatLng
